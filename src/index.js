24b2be79de89d3d54a660867c0016d822623e8ff
nikolayzhnr <mikalayzh@gmail.com>
2021-03-15 17:48:40 +0300

    typical array problems task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
+function isArrayFilled(array){
+  if(array.length != 0){
+    return 1;
+  } else{
+      return 0;
+  }
+}
+
+function isArrayNorm(array){
+  if(Array.isArray(array) == 1){
+    return isArrayFilled(array);
+  } else{
+      return 0;
+  }
+}
+
 
 exports.min = function min (array) {
-  return 0;
+  if(isArrayNorm(array) == 1){
+    array.sort((a,b) => a-b)
+    return array[0];
+  } else{
+      return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(isArrayNorm(array) == 1){
+    array.sort((a,b) => b-a);
+    return array[0];  
+  } else{
+      return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(isArrayNorm(array) == 1){
+    let summ = 0
+      , average = 0;
+
+    
+    for(let i=0 ; i < array.length ; i++){
+      summ += array[i];
+    }
+
+    
+
+    return summ/array.length;
+  } else{
+      return 0;
+  }
 }
+
+
